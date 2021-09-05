cc16a9c96ac939ee28138dbf13cde0e5adcad478
Hiren <waghmarehiren@gmail.com>
2021-09-05 11:47:11 +0530

    Changed Spelling Mistake.

--- src/com/nisren/fibonacci/Main.java
+++ src/com/nisren/fibonacci/Main.java
@@ -1,4 +1,4 @@
-package com.nisren.fibinacci;
+package com.nisren.fibonacci;
 
 public class Main {
     /**
@@ -6,28 +6,28 @@ public class Main {
      * 8=5+3
      * 13=8+5
      * f(n)=f(n-1)+f(n-2);
-     * 1.Unintentional/out of syllabus
+     * 1.Unintentional/Invalid input
      * 2.Base condition
      * 3.Recursion Case
      */
     public static void main(String[] args) {
 
         Main recursion = new Main();
-        var printValue = recursion.fibonacci(8);
-        //int printValue = recursion.fibonacci(5);
+        int printValue = recursion.fibonacci(4);
         System.out.println(printValue);
-
     }
 
     public int fibonacci(int number) {
+        //Handle Invalid/Unintentional Input
         if (number < 0) {
             return -1;
         }
+        //Base Case
         if (number == 0 || number == 1) {
             return number;
         }
+        //Recursive Case
         return fibonacci(number - 1) + fibonacci(number - 2);
-
     }
 }
 
